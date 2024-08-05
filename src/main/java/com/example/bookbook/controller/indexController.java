0f794363e95198323eff8b4ef1256fdc786bfccf
parkdohyun <wlswjqdmq1501@gmail.com>
2024-08-05 11:07:01 +0900

    feat: "기본설정"

---
+++ src/main/java/com/example/bookbook/controller/indexController.java
@@ -0,0 +1,23 @@
+package com.example.bookbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class indexController {
+	
+	@GetMapping
+	public String main() {
+		return "views/index/index.html";
+	}
+	@GetMapping("/bookList")
+	public String list() {
+		return "views/index/bookList.html";
+	}
+	
+	
+}
---
+++ src/test/java/com/example/bookbook/BookbookbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookbookbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
