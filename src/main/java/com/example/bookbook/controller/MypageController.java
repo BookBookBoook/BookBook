a7f5efcf3c57cb57979b76b0138f85aea66dcd4e
kdy <sksl6134@gmail.com>
2024-08-06 13:03:43 +0900

    feat:마이페이지-회원정보, 회원탈퇴

--- src/main/java/com/example/bookbook/controller/MypageController.java
+++ src/main/java/com/example/bookbook/controller/MypageController.java
@@ -12,9 +12,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class MypageController {
 	
 	@GetMapping("/mypage/orders")
-	public String mypage() {
+	public String order() {
 		return "views/mypage/order";
 	}
 	
+	@GetMapping("/mypage/orders/detail")
+	public String orderDetail() {
+		return "views/mypage/order-detail";
+	}
+	
+	@GetMapping("/mypage/account")
+	public String account() {
+		return "views/mypage/account";
+	}
+	
+	@GetMapping("/mypage/account/delete")
+	public String accountDel() {
+		return "views/mypage/account-delete";
+	}
 
 }
