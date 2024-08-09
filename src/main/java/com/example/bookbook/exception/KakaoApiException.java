2d64058cf3e498b6dc5c0ff147d122e8da807847
parkdohyun <wlswjqdmq1501@gmail.com>
2024-08-09 17:53:17 +0900

    feat:kakaoapi error

---
+++ src/main/java/com/example/bookbook/exception/KakaoApiException.java
@@ -0,0 +1,12 @@
+package com.example.bookbook.exception;
+
+import org.springframework.web.client.RestClientException;
+
+public class KakaoApiException extends RuntimeException {
+
+	public KakaoApiException(String message, RestClientException e) {
+		super(message,e);
+	}
+	
+
+}
---
+++ src/main/java/com/example/bookbook/service/impl/BookServiceProcess.java
@@ -0,0 +1,43 @@
+package com.example.bookbook.service.impl;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.bookbook.domain.dto.KakaoBookDto;
+import com.example.bookbook.mapper.BookMapper;
+import com.example.bookbook.service.BookService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceProcess implements BookService{
+	
+	
+	private final BookMapper bookMapper;
+	private final KakaoApiServiceProcess kakaoApiServiceProcess;
+	@Override
+	public void getAllBooks(Model model) {
+		List<KakaoBookDto> books = bookMapper.selectAll();
+		model.addAttribute("books", books);
+		
+	}
+	@Override
+	public void searchBooks(String query,int page, int size, Model model) {
+		List<KakaoBookDto> searchResults = kakaoApiServiceProcess.searchBooks(query, page, size);
+		model.addAttribute("books", searchResults);
+		model.addAttribute("query", query);
+		
+	}
+
+	
+
+
+
+
+}
