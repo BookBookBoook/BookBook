4d7928ae5b4511c74f1cb9e563316d5c166a095b
parkdohyun <wlswjqdmq1501@gmail.com>
2024-08-09 17:53:17 +0900

    feat:kakaoapi error

--- src/main/java/com/example/bookbook/controller/IndexController.java
+++ src/main/java/com/example/bookbook/controller/IndexController.java
@@ -1,11 +1,12 @@
 package com.example.bookbook.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.bookbook.service.main.KakaoBookService;
+import com.example.bookbook.service.BookService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,8 +15,9 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class IndexController {
-	//카카오api 서비스
-	private final KakaoBookService kakaoBookService;
+	
+	//책 서비스
+	private final BookService bookService;
 	
 	//메인페이지 이동
 	@GetMapping
@@ -23,9 +25,18 @@ public class IndexController {
 		return "views/index/index.html";
 	}
 	
+	//검색페이지
+	@GetMapping("/search")
+	public String searchBooks(@RequestParam String query,int page,int size, Model model) {
+		bookService.searchBooks(query,page,size,model);
+		return "views/index/searchResults";
+	}
+	//검색
+	
 	//도서목록페이지
 	@GetMapping("/bookList")
-	public String list() {
+	public String listBooks(Model model) {
+		bookService.getAllBooks(model);
 		return "views/index/bookList.html";
 	}
 	//도서상세페이지
