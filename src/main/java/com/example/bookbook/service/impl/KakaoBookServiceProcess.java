10fdc6a12e6ec98fa3ed02361e14ca50cc7a2129
parkdohyun <wlswjqdmq1501@gmail.com>
2024-08-09 14:27:49 +0900

    feat:serviceProcess clear

---
+++ src/main/java/com/example/bookbook/service/impl/KakaoBookServiceProcess.java
@@ -0,0 +1,10 @@
+package com.example.bookbook.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.example.bookbook.service.main.KakaoBookService;
+
+@Service
+public class KakaoBookServiceProcess implements KakaoBookService{
+
+}
