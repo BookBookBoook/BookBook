f6e531c87d41c9d3d4391b163871ab36e5890b0a
kdy <sksl6134@gmail.com>
2024-08-13 00:41:13 +0900

    feat: mybatis-qna

--- src/main/java/com/example/bookbook/mapper/QNAMapper.java
+++ src/main/java/com/example/bookbook/mapper/QNAMapper.java
@@ -4,13 +4,15 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.example.bookbook.domain.dto.QNADTO;
 import com.example.bookbook.security.CustomUserDetails;
 
 @Mapper
 public interface QNAMapper {
-
-	List<QNADTO> findAll(@Param("user") CustomUserDetails user);
+	
+	@Select("select * from qna where user_id = #{userId} order by qna_num desc")
+	List<QNADTO> findAll(@Param("userId") long userId);
 	
 }
