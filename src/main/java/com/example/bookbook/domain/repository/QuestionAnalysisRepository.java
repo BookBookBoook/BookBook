109c529551b359198ade2e99c6ec4721d85bff6d
jyj <hikenny96@naver.com>
2024-08-13 10:07:59 +0900

    feat:

--- src/main/java/com/example/bookbook/domain/repository/QuestionAnalysisRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.bookbook.domain.repository;
-
-import com.example.bookbook.domain.entity.QuestionAnalysis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuestionAnalysisRepository extends JpaRepository<QuestionAnalysis, Long> {
-}
