5fab6512b34923e857af0fba1f0878387bd544d7
jyj <hikenny96@naver.com>
2024-08-13 17:58:58 +0900

    feat:

---
+++ src/main/java/com/example/bookbook/domain/entity/KeywordEntity.java
@@ -0,0 +1,29 @@
+package com.example.bookbook.domain.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="keyword")
+public class KeywordEntity {
+	
+	@Id
+    private String keyword;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "intentionNo", nullable = false)
+    private IntentionEntity intention;
+}
