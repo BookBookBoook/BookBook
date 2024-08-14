e4c72d1087eabde54f859335bfb93442bbc8b0e0
u <mes0517@naver.com>
2024-08-14 13:09:00 +0900

    feat:inquiry

--- src/main/java/com/example/bookbook/mapper/InquiryMapper.java
+++ src/main/java/com/example/bookbook/mapper/InquiryMapper.java
@@ -3,14 +3,23 @@ package com.example.bookbook.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
 import com.example.bookbook.domain.dto.InquiryDTO;
+import com.example.bookbook.domain.dto.InquiryDetailDTO;
 
 @Mapper
 public interface InquiryMapper {
 
 	@Select("select * from qna order by qna_num desc")
 	List<InquiryDTO> findAll();
+
+	@Select("select * from qna where qna_num=#{qnaNum}")
+	InquiryDTO findInquiry(@Param("qnaNum") long qnaNum);
+
+	@Select("select * from qna_answer where qna_num=#{qnaNum}")
+	InquiryDetailDTO findDetail(@Param("qnaNum") long qnaNum);
+
 	
 }
