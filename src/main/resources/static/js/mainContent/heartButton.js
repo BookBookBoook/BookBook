fd9dde49081bb2e78f2bea1e6152a71d3f8eb097
parkdohyun <wlswjqdmq1501@gmail.com>
2024-08-14 14:23:31 +0900

    feat: book data save

--- src/main/resources/static/js/mainContent/heartButton.js
+++ src/main/resources/static/js/mainContent/heartButton.js
@@ -1,34 +1,29 @@
-$(document).ready(function() {
-    $('.heart-button').on('click', function() {
-        var $button = $(this);
-        var $bookItem = $button.closest('li');
-        var bookData = {
-            isbn: $bookItem.find('a[name="title"]').attr('href').split('/').pop(),
-            title: $bookItem.find('a[name="title"]').text(),
-            author: $bookItem.find('a[name="authors"]').text(),
-            price: $bookItem.find('h5[name="price"]').text(),
-            description: $bookItem.find('h6[name="content"]').text(),
-            image: $bookItem.find('img[name="thumbnail"]').attr('src')
-        };
+function addToFavorites(form, event) {
+     event.preventDefault();
+    const isbn = form.querySelector('input[name="isbn"]').value;
+    const csrfToken = document.querySelector('meta[name="_csrf"]').content;
+    const csrfHeader = document.querySelector('meta[name="_csrf_header"]').content;
 
-        $.ajax({
-            url: '/api/favorites',
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(bookData),
-            success: function(response) {
-                console.log('Success:', response);
-                alert('책이 성공적으로 즐겨찾기에 추가되었습니다!');
-                
-                // 하트 버튼 스타일 변경
-                $button.addClass('active');
-                $button.find('i').removeClass('far').addClass('fas');
-            },
-            error: function(xhr, status, error) {
-                console.error('Error:', error);
-                var errorMessage = xhr.responseText ? xhr.responseText : '알 수 없는 오류가 발생했습니다.';
-                alert('즐겨찾기 추가 중 오류가 발생했습니다. ' + errorMessage);
-            }
-        });
+    console.log("Client: Sending request to add book to favorites: " + isbn);
+
+    fetch(form.action, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            [csrfHeader]: csrfToken
+        },
+        body: new URLSearchParams(new FormData(form))
+    })
+    .then(data => {
+        console.log('Client: Success:', data);
+        // 성공 시 UI 업데이트
+        form.querySelector('.heart-button i').classList.remove('far');
+        form.querySelector('.heart-button i').classList.add('fas');
+    })
+    .catch((error) => {
+        console.error('Client: Error:', error);
+        alert('즐겨찾기 추가 중 오류가 발생했습니다: ' + error.message);
     });
-});
+
+    return false;
+}
